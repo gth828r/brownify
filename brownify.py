42baa90590f0c6dc04b19369af004b06acf13b96
Tim Upthegrove <tim.upthegrove@gmail.com>
2022-03-13 18:33:11 +0900

    Refactor brownify script for logging
     * As we move away from letting exceptions bubble up to the user, we
       need to explicitly provide return values from our main function and
       set the return code of the program using the sys module
     * Add a command line argument to set the log level
     * Update the recipe and recipe-file arguments such that they are
       mutually exclusive but required within argparse, removing the need
       to provide extra logging and to ensure usage is printed
     * Refactor intermediate file cleanup into its own method
       * Always allow cleanup to continue when errors occur, but log those
         errors atthe warning level
       * Allow for stack traces to be provided at the debug level
     * Refactor the main method so that most of the logic is in a try block
       which excepts all BrownifyErrors
       * All exceptions should eventually be caught by this logic so that
         logging can be performed in an appropriate manner
         * By default, all messages are logged at the error level
         * At the debug level, stack traces and full exception chains will
           be logged
         * If an exception occurs, the return value will always be non-zero
       * Add a finally block which invokes the cleanup method, ensuring that
         cleanup always is attempted
     * Instead of the variable tmp_name, use session_id, which is a more
       descriptive representation of the value
    
    Addresses #3

--- brownify.py
+++ brownify.py
@@ -1,11 +1,18 @@
 import argparse
+import sys
 from brownify.downloaders import YoutubeDownloader
+from brownify.errors import BrownifyError, InvalidInputError
 from brownify.parsers import ActionParser
 from brownify.splitters import AudioSplitterFactory, AudioSplitterType
 from brownify.runners import PipelineProcessor
+import logging
 import os
 import shutil
 import uuid
+
+
+_LOG_LEVELS = {"debug", "info", "warning", "error", "critical"}
+_DEFAULT_LOG_LEVEL = "warning"
 
 
 def get_args() -> argparse.Namespace:
@@ -14,67 +21,116 @@ def get_args() -> argparse.Namespace:
     parser.add_argument(
         "output", help="Filename to send output to (will be an mp3)"
     )
-    parser.add_argument("--recipe", help="Sequence to apply to audio streams")
     parser.add_argument(
+        "--log",
+        default=_DEFAULT_LOG_LEVEL,
+        choices=_LOG_LEVELS,
+        help="Log level to use for output",
+    )
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--recipe", help="Sequence to apply to audio streams")
+    group.add_argument(
         "--recipe-file", help="Path to an existing recipe file"
     )
 
     return parser.parse_args()
 
 
-def main():
-    args = get_args()
+def _get_log_level(log_level: str) -> int:
+    numeric_log_level = getattr(logging, log_level, None)
+    if not isinstance(numeric_log_level, int):
+        raise InvalidInputError(f"Invalid log level: {log_level}")
+    return numeric_log_level
 
-    youtube_url = args.youtube
-    output_file = args.output
-    recipe = args.recipe
-    recipe_file = args.recipe_file
 
-    program = None
+def _get_program(recipe, recipe_file) -> str:
     if recipe:
-        program = recipe
+        return recipe
 
-    elif recipe_file:
+    if recipe_file:
         with open(recipe_file, "r") as f:
             # Read in program and ignore newlines
             program = f.read().replace("\n", "")
+            return program
 
-    else:
-        raise Exception("Must provide either a recipe or a recipe file")
+    raise InvalidInputError("Either a recipe or a recipe file must be provided")
 
-    ap = ActionParser()
-    pipelines = ap.get_pipelines(program)
 
-    tmp_name = str(uuid.uuid4())
-    downloaded_file = f"{tmp_name}.mp4"
-
-    with YoutubeDownloader(youtube_url) as ytd:
-        ytd.get_audio(downloaded_file, file_type="mp4")
-
-    splitter = AudioSplitterFactory.get_audio_splitter(
-        AudioSplitterType.FIVE_STEM
-    )
-    splitter.split(downloaded_file)
-
+def _cleanup(downloaded_file, session_id):
     try:
+        # Clean the intermediate downloaded file
         os.remove(downloaded_file)
     except OSError as ose:
-        # FIXME: log here instead
-        raise ose
-
-    processor = PipelineProcessor(tmp_name, splitter)
-    processor.process(pipelines, output_file)
+        logging.warning(f"Could not remove downloaded file {downloaded_file}")
+        logging.debug(ose, exc_info=True)
 
     try:
         # Spleeter stores its files in a directory named after the original
         # file's base name
-        shutil.rmtree(str(tmp_name))
-        pass
-
+        shutil.rmtree(str(session_id))
     except OSError as ose:
-        # FIXME: log here instead
-        raise ose
+        logging.warning(f"Could not remove intermediate processed files under directory {session_id}/")
+        logging.debug(ose, exc_info=True)
+
+
+def main() -> int:
+    args = get_args()
+
+    youtube_url = args.youtube
+    output_file = args.output
+    log_level = args.log.upper()
+    recipe = args.recipe
+    recipe_file = args.recipe_file
+
+    # Put logging setup in its own block so if it fails, we can fail loudly
+    try:
+        log_level = _get_log_level(log_level)
+        logging.basicConfig(level=log_level)
+    except InvalidInputError as iie:
+        logging.error(iie)
+        return 1
+
+    # Create a random ID for tracking this session
+    session_id = str(uuid.uuid4())
+
+    # Create downloaded filename based on session ID
+    downloaded_file = f"{session_id}.mp4"
+
+    try:
+        # Get the program from user input
+        program = _get_program(recipe, recipe_file)
+
+        # Based on the program, instantiate a processing pipeline
+        ap = ActionParser()
+        pipelines = ap.get_pipelines(program)
+
+        # Grab an audio file based on the provided inputs
+        with YoutubeDownloader(youtube_url) as ytd:
+            ytd.get_audio(downloaded_file, file_type="mp4")
+
+        # Split the input audio into different tracks
+        splitter = AudioSplitterFactory.get_audio_splitter(
+            AudioSplitterType.FIVE_STEM
+        )
+        splitter.split(downloaded_file)
+
+        # Perform processing pipeline over the audio
+        processor = PipelineProcessor(session_id, splitter)
+        processor.process(pipelines, output_file)
+
+        return 0
+
+    except BrownifyError as be:
+        # All Brownify errors should log their message at the error level and
+        # log a stack trace at the debug level
+        logging.error(be)
+        logging.debug(be, exc_info=True)
+        return 1
+
+    finally:
+        # Clean up temporary files unless they have been marked for preservation
+        _cleanup(downloaded_file, session_id)
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
